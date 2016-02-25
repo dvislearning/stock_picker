ab4dcf56e2f142009f0800675457fab38dae6d1f
DV <dvislearning@gmail.com>
2016-02-25 10:06:57 -0800

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # stock_picker
+
+### An exercise in returning specific array values in Ruby from [The Odin Project](http://www.theodinproject.com/ruby-programming/building-blocks).  
+
+stock_picker takes one array parameter, representing the daily prices of a stock. An array is returned with the best price to buy a stock (the minimum value) and the best price to sell a stock (the maximum value).  
+
+prices[0] represents the first day.
+
+"Edge cases," such as when stock price is highest on the first day or when stock price is lowest on the last day were accounted for,
+
+Empty arrays, arrays with only one value, are also addressed.
+
+### Expected Outputs
+
+##### An empty array
+ stock_picker([])
+ 
+ ">> "No Data Available."
+
+##### Only one date entered
+ stock_picker([53])
+ 
+ ">> An investment strategy could not be formulated based on the information supplied."
+
+##### Lowest is last day.
+ stock_picker([12,3,15,22,14,17,1])
+ 
+ ">> "[1, 3] for a profit of $19"
+
+##### Highest day first.
+ stock_picker([55,11,13,11,18,11,22])
+ 
+ ">> "[1, 6] for a profit of $11"
+
+##### Exercise Example.
+ stock_picker([17,3,6,9,15,8,6,1,10])
+ 
+ ">> "[1, 4] for a profit of $12"
+
+##### No apporiate time to buy.
+ stock_picker([19,19,19,17,17,17])
+ 
+ ">> "An investment strategy could not be formulated based on the information supplied."
+
+##### Complex Array
+ stock_picker([66,65,64,63,55,17,3,6,9,15,21,3,8,2,6,21,3,1,10])
+ 
+ ">> "[13, 15] for a profit of $19" 
--- stock_picker.rb
+++ stock_picker.rb
@@ -1,27 +1,57 @@
-# prices = [53] Only one date entered
-# prices = [12,3,15,22,14,17,1] #lowest is last day
-# prices = [55,11,13,11,18,5,11,22]  #highest first day. 
-# prices = [17,3,6,9,15,8,6,1,10] #Exercise Example
+#input_prices = [] #Empty Array
+#input_prices = [53] #Only one date entered
+#input_prices = [12,3,15,22,14,17,1] #lowest is last day
+#input_prices = [55,11,13,11,18,11,22]  #highest first day. 
+input_prices = [17,3,6,9,15,8,6,1,10] #Exercise Example
+#input_prices = [19,19,19,17,17,17] #Set of prices where there is no apporiate time to buy.
+#input_prices = [66,65,64,63,55,17,3,6,9,15,21,3,8,2,6,21,3,1,10]  #crazy array
 
 
 
-def stock_picker (prices)
-	if prices.length <= 1
-		puts "Please enter 2 or more prices"
-		return nil
-end
-	if prices.max == prices[0] then prices.slice!(0) end
-	if prices.min == prices[-1] then prices.slice!(prices[-1]) end
-	if prices.length <= 1
-		puts "No suitable strategy can be formulated at this time."
-		return nil
-	else
-		max = prices.index(prices.max)
-		min = (prices[0..max]).index((prices[0..max]).min)
 
-		puts "#{[prices[min],prices[max]]} for a profit of $#{prices[max] - prices[min]}"
-		picks = [prices[min],prices[max]]
+def stock_picker (array)
+	b = [] # Contains index of all valid max numbers
+	palce_holder = []
+	valid_max = nil
+	max_finder = 0
+	min = nil
+	max = nil
+
+	if array.length == 0
+		puts "No Data Available."
+		return nil 
 	end
+
+	if array.max == array[0]
+		while array[max_finder] == (array[max_finder..array.length-1]).max
+			if (array[max_finder..array.length-1]).max == array.min
+				puts "An investment strategy could not be formulated based on the information supplied."
+				return nil
+			end
+			max_finder += 1
+		end
+		valid_max = array[max_finder..array.length-1].max
+	else 
+		valid_max = array.max
+	end
+
+	#unless valid_max == array.min
+
+	0.upto(array.length-1) do |x|
+		if array[x] == valid_max
+			b << x
+		end
+	end
+	
+	max = b.last
+
+	min = array.index(array[0..max].min)
+	puts "#{[min,max]} for a profit of $#{array[max] - array[min]}"
+	[min,max]
+
+
 end
 
-stock_picker(prices)
+
+stock_picker(input_prices)
+
